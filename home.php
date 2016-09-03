e12f23ecc256585ec646eff5ca148ec3125c4316
Lahiru Dssanyaka <lahirudiz@users.noreply.github.com>
2016-09-03 19:35:50 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>The Pastry Shop</title>
@@ -8,11 +8,103 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<?php
-   		include('header.php');
-		include('footer.php');
+   			include('header.php');
+			include('footer.php');
    		?>
-   </head>
+<style>
+.width {
+	width:100%;
+	height:20%;
+	}
+	
+.padding {
+	padding: 0px;
+	}
+	
+.pageContentWrapper{
+  margin-bottom:-23px;
+	}	
+.jumbotron{
+	margin-bottom: 0px;
+	 }	
+</style>
+
+</head>
 <body>
+<section class="width padding">
+
+  <div id="myCarousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li class="item1 active"></li>
+      <li class="item2"></li>
+      <li class="item3"></li>
+      <li class="item4"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="media\images\1.jpg" alt="Chania" width="1920" height="1080">
+      </div>
+
+      <div class="item">
+        <img src="media\images\2.jpg" alt="Chania" width="1920" height="1080">
+      </div>
+    
+      <div class="item">
+        <img src="media\images\3.jpg" alt="Flower" width="1920" height="1080">
+      </div>
+
+      <div class="item">
+        <img src="media\images\4.jpg" alt="Flower" width="1920" height="1080">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+</section>
+
+<script>
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel({interval: 2000, pause: "hover"});
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+</script>
+
+
+//////////////////////
 
 <div class="container">
 
