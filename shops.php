20aca33e3ebb20f24c699c70fc0d4dc1bcbc7e03
Lahiru Dssanyaka <lahirudiz@users.noreply.github.com>
2016-09-04 20:17:06 +0530

    Update shops.php

--- shops.php
+++ shops.php
@@ -8,11 +8,26 @@
    			include('header.php');
 			include('footer.php');
    		?>
-   </head>
+<style>
+.pageContentWrapper {
+margin-bottom:-23px;
+	}	
+.jumbotron {
+margin-bottom: 0px;
+	}
+.map {
+position:absolute;
+left:20%;	
+	}	
+</style>     
+        
+</head>
 <body>
-<div class="gmaps">	
+
+<div class="map">
 <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:440px;width:700px;'><div id='gmap_canvas' style='height:440px;width:700px;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="https://privacypolicytemplate.net">privacy policy template</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:13,center:new google.maps.LatLng(6.928101048851677,79.861414661377),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(6.928101048851677,79.861414661377)});infowindow = new google.maps.InfoWindow({content:'<strong>The Pastry Shop</strong><br>Colombo, Sri lanka<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
 </div>
+
 <p>Hotels</p>
 </body>
 </html>
